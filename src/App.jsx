67f1c85480795db898d8190a03ab9e7b4f142671
Rrudger <soulo@yandex.ru>
2024-09-26 19:31:06 +0200

    ugly simulation browser history

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './css/App.css';
 import i18n from './i18n';
 import Header from './components/Header.jsx';
@@ -10,17 +10,42 @@ import Offices from './components/Offices.jsx';
 
 const App = () => {
   const [page, setPage] = useState('landing');
-  const switchPage = () => {
-    if (page === 'offices') return <Offices switchPage={setPage} />
-    if (page === 'projects') return <Projects switchPage={setPage} />
-    if (page.includes('project_')) return <ProjectPage id={page.slice(-1)} switchPage={setPage} />
-    return <LandingPage switchPage={setPage} />
-  }
+  const switchPage = (nextPage) => {
+    const history = JSON.parse(sessionStorage.getItem('history')) || [];
+    if (history.length === 0) {
+      window.history.pushState(window.location.href, window.location.href);
+    }
+    history.push(page);
+    sessionStorage.setItem('history', JSON.stringify(history));
+    setPage(nextPage);
+  };
+
+  const renderPage = () => {
+    if (page === 'offices') return <Offices switchPage={switchPage} />
+    if (page === 'projects') return <Projects switchPage={switchPage} />
+    if (page.includes('project_')) return <ProjectPage id={page.slice(-1)} switchPage={switchPage} />
+    return <LandingPage switchPage={switchPage} />
+  };
+
+
+  window.onpopstate = function (e) {
+    e.preventDefault();
+    const history = JSON.parse(sessionStorage.getItem('history')) || [];
+    const prevPage = history.pop();
+    setPage(prevPage);
+    sessionStorage.setItem('history',JSON.stringify(history));
+    if (history.length !== 0) {
+      window.history.pushState(window.location.href, window.location.href);
+    } else {
+      location.reload();
+      setPage('landing')
+    }
+  };
 
   return (
     <div className='bg-secondary flex flex-col min-h-screen'>
-      <Header switchPage={setPage}/>
-      {switchPage(page)}
+      <Header switchPage={switchPage}/>
+      {renderPage(page)}
       <Footer />
     </div>
   );
