a5de5061eb2204c0dcf8e521867873716e7c8a24
Rrudger <soulo@yandex.ru>
2024-09-30 14:58:08 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
@@ -25,7 +26,15 @@ const config = {
         host: 'localhost',
     },
     plugins: [
-      new HtmlWebpackPlugin({ template: './public/index.html'})
+      new HtmlWebpackPlugin({ template: './public/index.html'}),
+      new CopyPlugin({
+      patterns: [
+        {
+        from: 'public/assets',
+          to: 'images'
+        }
+      ],
+    }),
     ],
     module: {
         rules: [
